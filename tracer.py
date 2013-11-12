685266eb6897ed0f8b2c54f787b3deef3ac977bf
Frank Singel <fjs52@case.edu>
2013-11-12 15:21:57 -0500

    first commit.

---
+++ tracer.py
@@ -0,0 +1,14 @@
+"""
+Created on Nov 12, 2013
+@author: Frank Singel
+FJS52@case.edu
+This module measures the TTL and hop count to a remote host
+"""
+
+import socket
+
+def main():
+	print socket.SOCK_DGRAM()
+
+if __name__ == "__main__":
+	main()
