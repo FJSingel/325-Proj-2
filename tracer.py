3091b5d201a7f61c32acae08315c5df5f0e83772
Frank Singel <fjs52@case.edu>
2013-11-12 18:14:15 -0500

    Wrote a basic framework that tries to access google. Not successfully though.

--- tracer.py
+++ tracer.py
@@ -8,7 +8,20 @@ This module measures the TTL and hop count to a remote host
 import socket
 
 def main():
-	print socket.SOCK_DGRAM()
+	HOST = "google.com"	#remote host name
+	PORT = 9001
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect((HOST, PORT))
+	s.sendall('Hello, world')
+	data = s.recv(1024)
+	s.close()
+	print 'Received', repr(data)
+
+	print socket.SOCK_DGRAM
+	#setsockopt() to change TTL field of datagram
+	#send it with socket.sendto()
+
 
 if __name__ == "__main__":
 	main()
